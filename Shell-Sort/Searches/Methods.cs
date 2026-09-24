e99273d9daedfb8e7f49ae7511aa11eb7a8c4753
Jess3yn <japeimbertd@gmail.com>
2026-09-24 09:54:36 -0600

    Shell Sort terminado
    Atte. JJ

---
+++ Shell-Sort/Searches/Methods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Searches
+{
+    internal class Methods<T>
+    {
+
+        public delegate bool criteria(T a, T b);
+
+
+        public void Shell(T[] array, criteria C)
+        {
+            int size = array.Length;
+            int half = size / 2;
+            bool change = false;
+            while(half > 1 || change == true)
+            {
+                change = false;
+                for (int i = 0; i < size - half; i++)
+                {
+                    if (C(array[i], array[i + half]))
+                    {
+                        T temporal = array[i];
+                        array[i] = array[i + half];
+                        array[i + half] = temporal;
+                        change = true;
+                    }
+                }
+                if(half > 1)
+                {
+                    half /= 2;
+                }
+            }
+            for(int i = 0; i < size; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Shell-Sort/Searches/Program.cs
@@ -0,0 +1,14 @@
+﻿using Searches;
+
+int[] test = {1000, 1, 50, 3000, 185, 67, 69, -6, -4, 38};
+
+Methods<int> sort = new Methods<int>();
+
+foreach (int i in test)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("------");
+
+sort.Shell(test, (int a, int b) => a > b);
